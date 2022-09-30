39bd530f1fa2dd242c5032c954e7006c38499655
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-10-01 01:02:23 +0400

    создали программу нахождение максимального числа из трёх заданных

---
+++ S4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.Clear();
+Console.WriteLine("Введите первое число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите второе число: ");
+int numberB = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите третье число: ");
+int numberC = int.Parse(Console.ReadLine()!);
+
+if (numberA > numberB && numberA > numberC)
+{ 
+    Console.WriteLine($"MAX = {numberA}");
+}
+else if (numberB > numberC && numberB > numberA)
+{
+    Console.WriteLine($"Max = {numberB}");
+}
+else if (numberC  > numberB && numberC > numberA)
+{
+    Console.WriteLine($"Max = {numberC}");
+}
