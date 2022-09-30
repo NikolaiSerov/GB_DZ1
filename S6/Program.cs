e9065a69852f0fcfc6b55a6d100a4515161554bc
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-10-01 01:08:36 +0400

    Создали программу определения четности числа

---
+++ S6/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт,
+// является ли число чётным (делится ли оно на два без остатка)
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int numberA = int.Parse(Console.ReadLine()!);
+
+if (numberA % 2 == 0)
+{ 
+    Console.WriteLine("ДА");
+}
+else 
+{
+    Console.WriteLine("НЕТ");
+}
