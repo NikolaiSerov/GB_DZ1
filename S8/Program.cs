817c40a3db23804d02c52ae012fa4f8a4cede9c0
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-10-01 01:21:00 +0400

    Создали программу, выводящую числовой массив четных чисел от 1 до N

---
+++ S8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Clear();
+Console.WriteLine("Введите число N: ");
+int numberA = int.Parse(Console.ReadLine()!);
+int i = 1;
+
+while (i <= numberA)
+{
+    if (i % 2 == 0)
+    Console.Write($"{i} ");
+    i = i + 1;
+}
