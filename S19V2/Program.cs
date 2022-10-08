5ad8812b77676004c0f6537ac3dd75eaea3fafc2
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-10-09 00:40:01 +0400

    программа определения палиндрома с использованием метода

---
+++ S19V2/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+int Palendrome(int num)
+{
+    int n = num;
+    
+       if (num / 10000 == num % 10 && (num % 10000) / 1000 == (num % 100) / 10)
+    {
+        Console.WriteLine($"Число {num} палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num} не палиндром");
+    }
+    return (num);
+}
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+Palendrome(N);
