13ee399602a3938f0f35e8ebeddd4786324d53b7
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-10-26 23:10:43 +0400

    Программа поиска индексов указанного числа в двумерном массиве

---
+++ S50/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает число и возвращает индексы элемента в двумерном массиве или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+bool FindNumbers(int[,] inArray, int Number, int rowIndex, int colmIndex)
+{
+    rowIndex = -1;
+    rowIndex = -1;
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (inArray[i, j] == Number)
+            {
+                rowIndex = i;
+                rowIndex = j;
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+Console.Write("Введите искомое число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 20);
+PrintArray(array);
+if (FindNumbers(array, number, rows, columns))
+{
+    Console.WriteLine($"Индексы искомого числа равны: [{rows}, {columns}]");
+}
+else
+{
+    Console.WriteLine($"{number} -> такого числа в массиве нет");
+}
