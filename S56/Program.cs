11c99ad99c9d902df5ef2e2c41217c67dd775732
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-11-15 00:34:31 +0400

    Программа нахождения строки массива с минимальной суммой элементов

---
+++ S56/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт 
+// номер строки с наименьшей суммой элементов: 1 строка
+
+
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MinSummNumRow(int[,] Array)
+{
+    int minRow = 0;
+    int minSummRow = 0;
+    int summRow = 0;
+    for (int i = 0; i < Array.GetLength(1); i++)
+    {
+        minRow += Array[0, i];
+    }
+    for (int i = 0; i < Array.GetLength(0); i++)
+    {
+        for (int j = 0; j < Array.GetLength(1); j++)
+        {
+            summRow += Array[i, j];
+        }
+        if (summRow < minRow)
+        {
+            minRow = summRow;
+            minSummRow = i;
+        }
+        summRow = 0;
+    }
+    Console.WriteLine($"Номер строки с наименьшей суммой элементов: {minSummRow + 1} строка");
+}
+
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+Console.WriteLine();
+
+MinSummNumRow(array);
