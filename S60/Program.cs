e38ea5d0ef4ccb1832d4948c0d630239c1c16d30
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-11-15 20:08:43 +0400

    программа, построчно выводящая  массив с индексы каждого элемента

---
+++ S60/Program.cs
@@ -0,0 +1,76 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+
+void GetArray(int[,,] array)
+{
+    int[] temp = new int[array.GetLength(0) * array.GetLength(1) * array.GetLength(2)];
+    int num;
+    for (int i = 0; i < temp.GetLength(0); i++)
+    {
+        temp[i] = new Random().Next(10, 100);
+        num = temp[i];
+        if (i >= 1)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                while (temp[i] == temp[j])
+                {
+                    temp[i] = new Random().Next(10, 100);
+                    j = 0;
+                    num = temp[i];
+                }
+                num = temp[i];
+            }
+        }
+    }
+    int count = 0;
+    for (int x = 0; x < array.GetLength(0); x++)
+    {
+        for (int y = 0; y < array.GetLength(1); y++)
+        {
+            for (int z = 0; z < array.GetLength(2); z++)
+            {
+                array[x, y, z] = temp[count];
+                count++;
+            }
+        }
+    }
+
+}
+
+void PrintArray(int[,,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]}({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+
+
+Console.Clear();
+Console.Write("Введите количество строк в массиве: ");
+int x = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество столбцов в массиве: ");
+int y = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество страниц в массиве: ");
+int z = int.Parse(Console.ReadLine()!);
+
+int[,,] matrix3D = new int[x, y, z];
+
+
+GetArray(matrix3D);
+PrintArray(matrix3D);
+
