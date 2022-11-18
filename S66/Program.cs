70d03b49f26cfe3b75a9d61e0deda1c1fff2005f
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-11-18 12:25:08 +0400

    программа подсчёта суммы натуральных элементов в промежутке от M до N

---
+++ S66/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int PrintNumbers(int start, int end)
+{
+    if (start == end)
+    {
+        return start;
+    }
+    // Console.WriteLine($"{start}");
+    return start + PrintNumbers(start + 1, end);
+}
+
+Console.Clear();
+Console.Write("Введите значение M: ");
+int M = int.Parse(Console.ReadLine()!);
+Console.Write("Введите значение N: ");
+int N = int.Parse(Console.ReadLine()!);
+int result = PrintNumbers(M, N);
+Console.WriteLine(result);
