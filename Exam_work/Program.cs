f9b9d724f38fccf512d7a29fa211dc472c224e0e
NikolaiSerov <ya.nikolaiserov@yandex.ru>
2022-11-28 23:12:17 +0400

    задание первоначального массива и объявление результирующего массива

---
+++ Exam_work/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+// длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, 
+// либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+// Примеры:
+// [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
+// [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
+// [“Russia”, “Denmark”, “Kazan”] → []
+void CheckElements3(string[] arraystart, string[] arrayend)
+{
+    int count = 0;
+    for (int i = 0; i < arraystart.Length; i++)
+    {
+        if (arraystart[i].Length <= 3)
+        {
+            arrayend[count] = arraystart[i];
+            count++;
+        }
+    }
+}
+
+Console.Clear();
+string[] Array = { "Hello", "2", "world", ":-)", "1234", "1567", "-2", "computer science" };
+string[] Result = new string[Array.Length];
